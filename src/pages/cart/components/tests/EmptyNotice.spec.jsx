4d9f89f7b2ea693ff32e607ce17eaf75bf952c0f
최영원 <57241273+foreverchoi0706@users.noreply.github.com>
2024-01-02 05:28:33 +0000

    modified:   src/pages/cart/components/tests/EmptyNotice.spec.jsx

--- src/pages/cart/components/tests/EmptyNotice.spec.jsx
+++ src/pages/cart/components/tests/EmptyNotice.spec.jsx
@@ -1,7 +1,19 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
 
+import { pageRoutes } from '@/apiRoutes';
 import EmptyNotice from '@/pages/cart/components/EmptyNotice';
 import render from '@/utils/test/render';
 
-it('"홈으로 가기" 링크를 클릭할경우 "/"경로로 navigate함수가 호출된다', async () => {});
+const navigate = vi.fn();
+
+vi.mock('react-router-dom', async () => {
+  const original = await vi.importActual('react-router-dom');
+  return { ...original, useNavigate: () => navigate };
+});
+
+it('"홈으로 가기" 링크를 클릭할경우 "/"경로로 navigate함수가 호출된다', async () => {
+  const { user } = await render(<EmptyNotice />);
+  await user.click(screen.getByRole('link'));
+  expect(navigate).toBeCalledWith(pageRoutes.main);
+});
